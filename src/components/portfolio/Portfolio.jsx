266e940269c3877e5ee3df97d95f978a8412b539
FinJoseph <126213184+FinJoseph@users.noreply.github.com>
2024-09-03 09:28:24 +0200

    Modifier

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -8,7 +8,7 @@ const items = [
         id:4,
         title:"Jeux Bibilava",
         img:"/24/bibilava.png",
-        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab placeat tempora maxime, facilis repellat debitis ipsam labore expedita cumque, quae laudantium. Similique quasi sequi sunt odit voluptates maxime dolorem! Delectus!",
+        desc:"J'ai développé un jeu classique de serpent en utilisant JavaScript, combinant des fonctionnalités simples et une interface utilisateur intuitive. Le jeu met en avant mes compétences en manipulation du DOM, en gestion des événements clavier, et en création de boucles de jeu fluides. Le but du jeu est de diriger le serpent pour manger la nourriture qui apparaît aléatoirement sur l'écran, tout en évitant de toucher les bords ou de se mordre la queue. Ce projet démontre ma capacité à coder des jeux interactifs tout en gardant une logique de programmation propre et efficace.",
         link:"http://jeux-bibilava.s3-website.eu-north-1.amazonaws.com/game.html"
     },
 
@@ -24,7 +24,7 @@ const items = [
         id:2,
         title:"Projet2",
         img:"/24/projet.png",
-        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab placeat tempora maxime, facilis repellat debitis ipsam labore expedita cumque, quae laudantium. Similique quasi sequi sunt odit voluptates maxime dolorem! Delectus!",
+        desc:"Ce portfolio est le reflet de mon parcours et de mes compétences en développement web et en design numérique. J'ai conçu et développé ce site pour mettre en avant mes projets, mes réalisations, et mes services. L'interface est pensée pour offrir une navigation fluide et agréable, tout en étant entièrement responsive pour s'adapter à tous les types d'écrans. Ce projet témoigne de ma maîtrise des technologies front-end modernes, ainsi que de mon souci du détail et de l'expérience utilisateur.",
     },
 
     {
